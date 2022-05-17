1dd23ede8a6d75372543c3a862234ec925872d91
Fernando.M <maisanche45@hotmail.com>
2022-05-17 02:07:31 +0200

    añadir

---
+++ src/app/Models/Customer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+
+class Customer extends Model
+{
+    use HasApiTokens, HasFactory, Notifiable;
+    protected $fillable = [
+        'nombre','apellidos', 'email', 'numero', 'direccion', 'ciudad', 'codigopostal', 'pais', 'municipio', 'titular_tarjeta', 'numero_tarjeta', 'mes_expiracion_tarjeta', 'anho_expiracion_tarjeta', 'cvv'
+    ];
+
+}
